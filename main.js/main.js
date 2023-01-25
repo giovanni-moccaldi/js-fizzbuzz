3ae25555f6d3f7d1ba194470aba187f48e1a9c8b
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-25 15:46:49 +0100

    buzz

--- main.js/main.js
+++ main.js/main.js
@@ -1,6 +1,14 @@
 // stampare in console numeri da 1 a 100
+const listtowrite = document.getElementById("list");
+
 for (let i = 1; i < 100; i++) {
-    console.log(i)
+
+    if (i % 3 == 0)
+        console.log(i)
+    listtowrite.innerHTML += "Fizz";
+    if (i % 5 == 0)
+        console.log(i)
+    listtowrite.innerHTML += "Buzz";
 }
 
 // poi stampa  i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
