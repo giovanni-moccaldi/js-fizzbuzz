ed7a574b620cd65e3b9ec1cf3c1ea1ecd98f787a
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-25 14:08:07 +0100

    start

---
+++ main.js/main.js
@@ -0,0 +1,3 @@
+// stampare in console numeri da 1 a 100
+// poi stampa  i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
+// poi per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz
