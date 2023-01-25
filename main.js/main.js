b83793f212f269e4a090c63fe4a721fabe2fc4fc
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-25 15:06:35 +0100

    1step

--- main.js/main.js
+++ main.js/main.js
@@ -1,3 +1,7 @@
 // stampare in console numeri da 1 a 100
+for (let i = 1; i < 100; i++) {
+    console.log(i)
+}
+
 // poi stampa  i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
 // poi per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz
