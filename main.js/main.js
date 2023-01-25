723e6bceb536681d5cab213280d1d0d305a79061
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-25 17:23:19 +0100

    fizzbuzz

--- main.js/main.js
+++ main.js/main.js
@@ -1,15 +1,40 @@
 // stampare in console numeri da 1 a 100
 const listtowrite = document.getElementById("list");
 
-for (let i = 1; i < 100; i++) {
 
-    if (i % 3 == 0)
+for (let i = 1; i < 100; i++) {
+    const listitem = document.createElement("li");
+
+    if (i % 3 == 0) { //poi stampa  i multipli di 3 stampi in console "Fizz
+        console.log(i);
+        listtowrite.innerHTML += "Fizz";
+        listitem.classList.add("red");
+        listtowrite.append(listitem);
+    }
+    else if (i % 5 == 0) { // " al posto del numero e per i multipli di 5 stampi "Buzz".
         console.log(i)
-    listtowrite.innerHTML += "Fizz";
-    if (i % 5 == 0)
+        listtowrite.innerHTML += "Buzz";
+
+        listitem.classList.add("blu");
+        listtowrite.append(listitem);
+
+    }
+
+    else if (i % 5 % 3 == 0) { // poi per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz
         console.log(i)
-    listtowrite.innerHTML += "Buzz";
+        listtowrite.innerHTML += "fizz99uzz";
+
+        listitem.classList.add("green");
+        listtowrite.append(listitem);
+
+
+    }
+    else {
+        listtowrite.innerHTML += i;
+        listitem.classList.add("grey");
+        listtowrite.append(listitem);
+
+    }
+
 }
 
-// poi stampa  i multipli di 3 stampi in console "Fizz" al posto del numero e per i multipli di 5 stampi "Buzz".
-// poi per i numeri che sono sia multipli di 3 che di 5 stampi in console "FizzBuzz
